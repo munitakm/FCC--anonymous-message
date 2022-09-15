c3b0d46a783c39e3e3e2594d25c0b335c564dc88
munitakm <renan.manshio@gmail.com>
2022-09-15 19:40:46 -0300

    fixed a script on board.html

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,13 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+	test('POST request to /api/threads/{board}', (done) => {
+		chai.request(server)
+		.post('/api/threads/:board')
+		.send({board: "teste", text: "teste", delete_password: "delete_me"})
+			.end((err, res) => {
+				assert.equal(res.status, 200),
+				done();
+			})
+	})
 });
