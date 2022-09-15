058242057fb3687419dc5087fb7ad6a928b97399
munitakm <renan.manshio@gmail.com>
2022-09-15 19:31:03 -0300

    trying to write the routes not sure if is gone work at the final

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@
 const url = require('url')
 const mongoose = require('mongoose');
 const {Schema} = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 module.exports = function (app) {
 //Mongoose Schema and Model Database
@@ -9,79 +10,126 @@ module.exports = function (app) {
 			"text": String,
 			"delete_password": String,
 			"board": String,
-			"created_on": {type: Date, default: new Date().toUTCString()},
+			"created_on": {type: Date},
 			"reported": {type: Boolean, default: false}
 	});
-	const Replies = mongoose.model("Replies", repliesSchema);
+	const Replie = mongoose.model("Replies", repliesSchema);
 
 	const threadSchema = mongoose.Schema({
 		"board": String,
 		"text": String,
-		"delete_password": String,
-		"created_on": {type: Date, default: new Date().toUTCString()},
-		"bumped_on": {type: Date, default: new Date().toUTCString()},
+		"delete_password": {type: String},
+		"created_on": {type: Date},
+		"bumped_on": {type: Date},
 		"reported": {type: Boolean, default: false}, 
-		"replies": [repliesSchema],
+		"replies": [],
 	});
 
 	const Thread = mongoose.model("Thread", threadSchema);
+//--------------------------------------
 
-//Send POST request to/api/thread/{board}	
-
-  app.post('/api/threads/:board', (req, res) => {
-	console.log(req.body);
-	const newThread = new Thread(req.body);
-	  Thread.create(newThread, (err, saved) => {
-		if( !err && saved )
-		  return res.redirect(url.format({
-		pathname: `/b/${req.body.board}`,
-		query: newThread,
-		}))
-	})
-	});
-//POST to /api/replies/{board}
-	app.post('/api/replies/:board', async (req, res) => {
-		console.log(req.body);
-		const find = await Thread.findById(req.body.thread_id)
-		if(find) {
-		console.log(req.body);
-			const time = new Date().toUTCString();
-			const newReplie = new Replies(req.body);
-			newReplie.created_on = time
-			Thread.findOneAndUpdate({
-				_id: req.body.thread_id},
-				{$push: {replies: newReplie}, $set: {bumped_on: time}},
-				(err, res) => {
-				if(err) console.log(err);
-			})
-	}
-//GET request to /api/threads/{board}
-		})
-   
-	app.get('/api/threads/:board', (req, res) => {
-		Thread.find({board: req.params.board})
-		.sort({bumped_on: 'desc'})
-		.limit(10)
-		.select(['-delete_password', '-reported'])
-			.exec((err, list) => {
-				list.forEach((thread) => {
-					thread.replies.sort((target1, target2) => {
-						return target2.created_on - target1.created_on;
-					})
-					thread.replies = thread.replies.slice(0, 3);
-					thread.replies.forEach((target) => {
-						target.reported = undefined;
-						target.created_on = undefined
-					})
-				})
-				res.json(list)
-			})
-
-	})
-  
+//Threads Route
   app.route('/api/threads/:board')
-		
-  app.route('/api/replies/:board');
+	//POST request
+	.post(async(req, res) => {
+		try {
+			let board = req.params.board;
 
-};
-  
+			let newThread = await Thread.create({
+				board: board,
+				text: req.body.text,
+				created_on: new Date().toUTCString(),
+				bumped_on: new Date().toUTCString(),
+				delete_password: await bcrypt.hash(req.body.delete_password, 8),
+				reported: false,
+				replies: []
+			});
+				return res.redirect(`/b/${board}`)
+		}
+		catch (err) {
+				console.log(err)
+				return res.json({"message": "error"})
+		}
+	})
+	//GET request
+		.get(async (req, res) => {
+			try {
+				let board = req.params.board;
+				await Thread.find({board: board})
+				.sort({bumped_on: 'desc'})
+				.limit(10)
+				.lean()
+					.exec((err, list) => {
+						if(!err && list) {
+							list.delete_password = undefined;
+							list.reported = undefined;
+							list.forEach(i => {
+								i.replycount = i.replies.length;
+
+								i.replies.sort((a,b) => {
+									return b.created_on - a.created_on;
+								});
+								i.replies = i.replies.slice(0,3);
+								i.replies.sort((a, b) => {
+									return a.created_on - b.created_on;
+								})
+								i.delete_password = undefined;
+								i.reported = undefined;
+							});
+							return res.json(list);
+						}
+					});
+			} catch (err) {
+				return res.json({"message": "error get threads"})
+			}
+		})
+//Replies Route	
+  app.route('/api/replies/:board')
+	//POST request
+	.post(async(req,res) => {
+		console.log(req.body, req.params);
+		try {
+			let board = req.params.board;
+			let {text, delete_password, thread_id} = req.body;
+			let newReplie = new Replie({
+				text: text,
+				created_on: new Date(),
+				reported: false,
+				delete_password: await bcrypt.hash(delete_password, 8)
+			});
+			await Thread.findOne({_id: thread_id, board: board})
+				.exec(async (err, thread) => {
+					if(!err && thread) {
+						thread.replies.push(newReplie);
+						thread.bumped_on = newReplie.created_on;
+						await thread.save();
+						console.log("saved replie");
+						res.redirect(`/b/${board}/${thread_id}`)
+					}
+				});
+
+			} catch(err) {
+				console.log(err)
+				res.json({ "message": "error post replie" })
+			}
+	
+		})
+	//GET request
+		.get((req, res) => {
+		console.log(req.query, req.params);
+			Thread.findById(req.query.thread_id)
+				.lean()
+				.exec((err, found) => {
+				if(!err && found) {
+					found.delete_password = undefined;
+					found.reported = undefined;
+					found.replies.forEach(i => {
+						i.delete_password = undefined;
+						i.reported = undefined;
+					});
+					console.log(found)
+					res.json(found)
+				}
+			})
+		})
+}
